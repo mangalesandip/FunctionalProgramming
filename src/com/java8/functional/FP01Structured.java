89ff61d4e78b8d48018110d8ac98d0e5504eaa11
sandip.mangale <sandip.m3@gmail.com>
2021-10-08 23:10:22 +0530

    structured

--- src/com/java8/functional/FP01Structured.java
+++ src/com/java8/functional/FP01Structured.java
@@ -5,19 +5,12 @@ import java.util.List;
 
 public class FP01Structured {
 	public static void main(String[] args) {
-		printAllNumbersInListStructure(Arrays.asList(45,56,7,88));
+		printAllNumbersInListStructure(Arrays.asList(45,56,70,88));
 		
 	}
 
-	private static void print(int number) {
-		System.out.println(number);
-	}
 	private static void printAllNumbersInListStructure(List<Integer> numbers) {
-		numbers.stream().forEach(FP01Structured :: print);
-		
-		
-//		for (Integer number : numbers) {
-//			System.out.println(number);
-//		}
+		numbers.stream()
+			.forEach(System.out :: println);// Method reference
 	}
 }

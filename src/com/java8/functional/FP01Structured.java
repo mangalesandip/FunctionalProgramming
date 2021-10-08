3167074572bf9de7c7a0761c561640880ae26e67
sandip.mangale <sandip.m3@gmail.com>
2021-10-08 23:02:24 +0530

    added sample program to print number

--- src/com/java8/functional/FP01Structured.java
+++ src/com/java8/functional/FP01Structured.java
@@ -1,7 +1,17 @@
 package com.java8.functional;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FP01Structured {
 	public static void main(String[] args) {
-		System.out.println("First java8 program");
+		printAllNumbersInListStructure(Arrays.asList(45,56,7,88));
+		
+	}
+
+	private static void printAllNumbersInListStructure(List<Integer> numbers) {
+		for (Integer number : numbers) {
+			System.out.println(number);
+		}
 	}
 }

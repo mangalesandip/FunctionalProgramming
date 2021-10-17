3fcb4f5e00040ebad66fc39dea0181831cf7ce0a
sandip.mangale <sandip.m3@gmail.com>
2021-10-17 11:44:05 +0530

    added example for method reference

--- src/io/java8/predicate/ThisRefExample.java
+++ src/io/java8/predicate/ThisRefExample.java
@@ -4,24 +4,30 @@ public class ThisRefExample {
 	public void doProcess(int i, Process p) {
 		p.process(i);
 	}
-	public static void main(String[] args) {
-		ThisRefExample thisRefExample =new ThisRefExample();
-		thisRefExample.doProcess(10, new Process() {
-			@Override
-			public void process(int i) {
-				System.out.println("i=>"+ i);
-				System.out.println(this);
-			}
-			
-			public String toString() {
-				return "This is annoumous inner class";
-			}
+	
+	public void execute() {
+		doProcess(50, i ->{
+			System.out.println("i=>" + i);
+			System.out.println(this);
 		});
+	}
+
+	public static void main(String[] args) {
+		ThisRefExample thisRefExample = new ThisRefExample();
+
+//		thisRefExample.doProcess(55, i -> {
+//			System.out.println("i=>" + i);
+//		});
 		
+		thisRefExample.execute();
 	}
 	
+	public String toString() {
+		return "This ref.";
+	}
+
 }
 
-interface Process{
+interface Process {
 	void process(int i);
 }
---
+++ src/io/java8/unit3/MethodReferenceExample1.java
@@ -0,0 +1,15 @@
+package io.java8.unit3;
+
+public class MethodReferenceExample1 {
+	public static void main(String[] args) {
+		Thread t = new Thread(()->pringMsg());
+		t.start();
+		
+		Thread t1 = new Thread(MethodReferenceExample1 :: pringMsg);
+		t1.start();
+		
+	}
+	public static void pringMsg() {
+		System.out.println("This method ref.");
+	}
+}
